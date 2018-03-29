951a72088579e6d680aa9492c76f8bd65550583f
Jandré Pretorius <jandrepretorius89@users.noreply.github.com>
2018-03-29 08:37:09 +0200

    Update smite.py
    remove ids

--- smite.py
+++ smite.py
@@ -23,8 +23,8 @@ for x in range(0, 10000):
 
             prev_val = "{} {}".format(element['user_name'], data)
 
-            channelID = "428596424515649536" # enable dev mode on discord, right-click on the channel, copy ID
-            botToken = "NDI4NTk5Mjg1MTk4ODgwNzcw.DZ1b-Q.5D5yYV2HvNRK8U3KLXGmj4eKcfo"    # get from the bot page. must be a bot, not a discord app
+            channelID = "" # enable dev mode on discord, right-click on the channel, copy ID
+            botToken = ""    # get from the bot page. must be a bot, not a discord app
 
             baseURL = "https://discordapp.com/api/channels/{}/messages".format(channelID)
             headers = { "Authorization":"Bot {}".format(botToken),
